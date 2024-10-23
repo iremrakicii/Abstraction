fb74fc326f55b179c28ded983ebba0418801c73e
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-23 14:26:46 +0300

    Proje dosyası ekle.

---
+++ Abstraction/Company.cs
@@ -0,0 +1,59 @@
+﻿namespace Abstraction
+{
+
+    public abstract class Company
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+        public abstract void Gorev();
+
+
+        public Company(string name, string lastName, string department)
+        {
+            Name = name;
+            LastName = lastName;
+            Department = department;
+        }
+    }
+    public class ProjectManager : Company
+    {
+        public ProjectManager(string name, string lastName) : base(name, lastName, "Proje Yöneticisi")
+        {
+
+        }
+
+
+        public override void Gorev()
+        {
+            Console.WriteLine("Şirkette Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+    public class SoftwareDeveloper : Company
+    {
+        public SoftwareDeveloper(string name, string lastName) : base(name, lastName, "Yazılım Geliştiricisi")
+        {
+
+        }
+        public override void Gorev()
+        {
+            Console.WriteLine("Şirkette Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+    public class SalesRepresentative : Company
+    {
+        public SalesRepresentative(string name, string lastName) : base(name, lastName, "Satış Temsilcisi")
+        {
+
+        }
+        public override void Gorev()
+        {
+            Console.WriteLine("Şirkette Satış Temsilcisi olarak çalışıyorum.");
+        }
+    }
+
+}
+
+
+
---
+++ Abstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using Abstraction;
+
+class Program
+{
+    static void Main()
+    {
+        Company projectManager = new ProjectManager("Reha", "Rakıcı");
+        Console.WriteLine($"{projectManager.Name} {projectManager.LastName}, şirketin {projectManager.Department}");
+        projectManager.Gorev();
+        Console.WriteLine("--------------------------------------------------");
+        Company softwareDeveloper = new SoftwareDeveloper("İrem", "Rakıcı");
+        Console.WriteLine($"{softwareDeveloper.Name} {softwareDeveloper.LastName}, şirketin {softwareDeveloper.Department}");
+        softwareDeveloper.Gorev();
+        Console.WriteLine("--------------------------------------------------");
+        Company salesRepresentative = new SalesRepresentative("Tahsin", "Rakıcı");
+        Console.WriteLine($"{salesRepresentative.Name} {salesRepresentative.LastName}, şirketin {salesRepresentative.Department}");
+        salesRepresentative.Gorev();
+    }
+}
